20f26e0be68046235fcbc45123c333b3f035dc7f
ENGAhmedDahir <ahmeddahir381@gmail.com>
2026-03-08 18:37:04 +0300

    something changes

--- app/_components/ReservationContex.js
+++ app/_components/ReservationContex.js
@@ -6,9 +6,9 @@ const initialState = { from: undefined, to: undefined };
 
 function ReservationProvider({ children }) {
   const [range, setRange] = useState(initialState);
-  const restRange = () => setRange(initialState);
+  const resetRange = () => setRange(initialState);
   return (
-    <ReservationContext.Provider value={{ range, setRange, restRange }}>
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
       {children}
     </ReservationContext.Provider>
   );
